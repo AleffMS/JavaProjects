3240024f183aa762ea34ca8315fb8b19954fdb29
Aleff Martins <aleff.computacao2014@gmail.com>
2017-11-05 13:37:10 -0200

    Java, ws - curso

---
+++ CursoWebService/src/java/model/Aluno.java
@@ -0,0 +1,55 @@
+package model;
+
+/**
+ *
+ * @author aleff
+ */
+public class Aluno {
+
+    private int id;
+    private String nome;
+    private String telefone;
+    private String endereco;
+    private String curso;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+}
